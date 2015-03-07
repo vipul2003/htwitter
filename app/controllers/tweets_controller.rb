c7d99410c1e83fc2117c0500ad4d5cc61517e045
vipul2003 <vipul2003@gmail.com>
2015-03-07 14:09:18 -0500

    added params

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,13 @@ class TweetsController < ApplicationController
 	def new
 		@tweet = Tweet.new
 	end
+	def create
+		@tweet = Tweet.new(tweet_param)
+		@tweet.save
+		render 'new'
+	end
+
+	def tweet_param
+		params.require(:tweet).permit(:content)
+	end
 end
