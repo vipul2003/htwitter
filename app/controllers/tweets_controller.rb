a83821a1008fe96db5a87fee11cf22f6c71ef15b
vipul2003 <vipul2003@gmail.com>
2015-03-07 16:01:46 -0500

    created newsfeed

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,12 +5,18 @@ class TweetsController < ApplicationController
 	def create
 		@tweet = Tweet.new(tweet_param)
 		if @tweet.save
-			flash[:error] = "You have created a tweet"
+			flash[:success] = "You have created a tweet"
 			redirect_to new_tweet_path
 		end
+	end
+
+	def index
+		@tweets = Tweet.all
 	end
 
 	def tweet_param
 		params.require(:tweet).permit(:content)
 	end
+
+
 end
